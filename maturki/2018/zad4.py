e51c5cf8bbcc875c5deb5b6b3b419b648fb095fc
hubert <hubertszymanski5@gmail.com>
2023-02-07 18:27:02 +0100

    improved 2018 zad 4.3

--- maturki/2018/zad4.py
+++ maturki/2018/zad4.py
@@ -3,6 +3,7 @@ def main():
         arr = [x for x in dane.read().strip().split('\n')]
     zad1(arr)
     zad2(arr)
+    zad3_prev(arr)
     zad3(arr)
 
 
@@ -30,7 +31,7 @@ def zad2(arr):
     print(f'zad 4.2: {current_word} {max_diff_letters_count}')
 
 
-def zad3(arr):
+def zad3_prev(arr):
     words = []
     for word in arr:
         is_ok = True
@@ -41,9 +42,29 @@ def zad3(arr):
                     break
         if is_ok:
             words.append(word)
-    print(f'zad 4.3')
-    for word in words:
-        print(word)
+    print(f'zad 4.3: <uncomment to see :D>')
+    print(len(words))
+    # for word in words:
+    #     print(word)
+
+
+def zad3(arr):
+    words = []
+    for word in arr:
+        # sorted_letters = sorted(set(word))  # O(nlogn)
+        # first_letter = sorted_letters[0]
+        # last_letter = sorted_letters[-1]
+        # diff = ord(last_letter) - ord(first_letter)
+        min_val = 1000
+        max_val = 0
+        for char in word:
+            curr_val = ord(char)
+            min_val = curr_val if curr_val < min_val else min_val
+            max_val = curr_val if curr_val > max_val else max_val
+        diff = max_val - min_val
+        if diff <= 10:
+            words.append(word)
+    print(len(words))
 
 
 if __name__ == '__main__':

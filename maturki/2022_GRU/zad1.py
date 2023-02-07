0977dc14d0cb762af0851bcc83fb82be0b49cdc7
hubert <hubertszymanski5@gmail.com>
2023-02-07 17:28:39 +0100

    finish 2022_GRU

--- maturki/2022_GRU/zad1.py
+++ maturki/2022_GRU/zad1.py
@@ -1,27 +1,17 @@
 def main():
     file = open('Dane_2212/mecz.txt', 'r').read().strip()
-    print(mecz(file))
+    print(zad3(file))
 
 
-def mecz(file):
+def zad1(file):
     different_winners = 0
-    current_passa = 0
-    best_passa = 0
-    current_passa_team = ''
-    best_passa_team = ''
-    passa_count = 0
     for i in range(len(file)):
         if file[i] != file[i - 1]:
             different_winners += 1
-            if current_passa >= 10:
-                passa_count += 1
-            current_passa = 0
-        else:
-            current_passa += 1
-            current_passa_team = file[i]
-            if current_passa > best_passa:
-                best_passa = current_passa
-                best_passa_team = current_passa_team
+    return different_winners
+
+
+def zad2(file):
     a_wins = 0
     b_wins = 0
     current_winner = ''
@@ -29,17 +19,34 @@ def mecz(file):
         if char == 'A':
             a_wins += 1
             current_winner = 'A'
-            if abs(a_wins - b_wins) > 3 and a_wins >= 1000:
+            if a_wins - b_wins >= 3 and a_wins >= 1000:
                 break
         else:
             b_wins += 1
             current_winner = 'B'
-            if abs(a_wins - b_wins) > 3 and b_wins >= 1000:
+            if b_wins - a_wins >= 3 and b_wins >= 1000:
                 break
-    return f'odp zad1: {different_winners}, zad2: {a_wins, b_wins, current_winner} zad3: {passa_count, best_passa + 1, best_passa_team}'
+    return f'{current_winner} {a_wins}:{b_wins}'
 
+
+def zad3(file):
+    current_passa = 0
+    best_passa = 0
+    best_passa_team = ''
+    passa_count = 0
+    for i in range(len(file)):
+        if file[i] != file[i - 1]:
+            if current_passa >= 10:
+                passa_count += 1
+            current_passa = 1
+        else:
+            current_passa += 1
+            current_passa_team = file[i]
+            if current_passa > best_passa:
+                best_passa = current_passa
+                best_passa_team = current_passa_team
+    return f'{passa_count} {best_passa_team} {best_passa}'
 
 
 if __name__ == '__main__':
     main()
-    

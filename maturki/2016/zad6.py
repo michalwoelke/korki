fd9d981ddfc2208ec9e03bbfcc4fb915c241c86a
hubert <hubertszymanski5@gmail.com>
2023-05-15 18:57:03 +0200

    2016: zad 6 - finished

--- maturki/2016/zad6.py
+++ maturki/2016/zad6.py
@@ -9,9 +9,9 @@ def main():
     with open("dane/dane_6_3.txt") as dane3:
         arr3 = [[y for y in x.split(" ")] for x in dane3.read().strip().split("\n")]
     # print(arr2)
-    zad2(arr2)
-    # zad2(fix_arr2(arr2))
-    # print(zad3(arr3))
+    zad1(arr1)
+    zad2(fix_arr2(arr2))
+    zad3(arr3)
 
 
 def zad1(arr):
@@ -26,13 +26,33 @@ def zad1(arr):
 def zad2(arr):
     result = []
     for el in arr:
-        print(el)
         word = el[0]
         k = el[1]
         decrypted = decrypt(word, k)
         result.append(decrypted)
     print(f'zad 6.2: {result}')
     print(f'writing... {write_to_file("wyniki_6_2.txt", result)}')
+
+
+def zad3(arr3):
+    result = []
+    for pair in arr3:
+        if not is_valid_encryption(pair[0], pair[1]):
+            result.append(pair[0])
+    print(f'zad 6.3: {result}')
+    print(f'writing... {write_to_file("wyniki_6_3.txt", result)}')
+
+
+def is_valid_encryption(word, encrypted):
+    k = calculate_k(word[0], encrypted[0])
+    for i in range(1, len(word)):
+        if calculate_k(word[i], encrypted[i]) != k:
+            return False
+    return True
+
+
+def calculate_k(l1, l2):
+    return (ord(l1) - ord(l2)) % 26
 
 
 def fix_arr2(arr2):
@@ -46,11 +66,11 @@ def fix_arr2(arr2):
 
 
 def write_to_file(filename, content):
-    file = open(filename, 'w')
-    for el in content:
-        file.write(el + "\n")
-    file.close()
-    return "Done!"
+    with open(filename, 'w') as file:
+        for el in content:
+            file.write(el + "\n")
+        file.close()
+        return "Done!"
 
 
 def decrypt(word, k):
